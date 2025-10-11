37c6dd60567672c3a2fd589bc5dd7adc5c1f39ea
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-10-11 19:13:29 +0500

    embed php everywhere you want like html and javascipt, this scriping language so you can make slots or junks of code everywhere

--- embed_php_using_scripts.php
+++ embed_php_using_scripts.php
@@ -8,3 +8,18 @@ $name = "Sohail Manzoor";
 
 echo $name;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Sohail"; echo "Manzoor";?></title>
+</head>
+<body>
+    <br>
+    <br>
+     <?php echo "Sohail Manzoor"?>
+</body>
+</html>
+
