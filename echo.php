07fc989db95ae5cc81e6b2bbc932280a30ce891c
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-10-11 18:26:50 +0500

    output my name to the brower using echo command - first php program

---
+++ echo.php
@@ -0,0 +1,10 @@
+<?php // php starting tag
+//   PHP stands for:
+//➡️ Hypertext Preprocessor
+// PHP is a powerful backend language that makes websites interactive, database-driven, and dynamic.
+
+// echo is a PHP command used to output text to the browser.
+echo "SohAil MAnzoor - Mobile Application Developer"
+
+// php ending tag
+?>
