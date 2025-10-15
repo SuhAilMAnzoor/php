da5effa86974e4444026565900f090540bc1055e
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-10-15 17:22:20 +0500

    variables in php

---
+++ variables.php
@@ -0,0 +1,16 @@
+<?php
+    echo "talking about variables, <br>" ;
+    echo '<br>';
+// Variable are containers like you store information
+// Starts with a $ like $name
+// variables are case sensitive
+// php automatically detect what datatype of variable when you assign the value 
+$name = "Sohail Manzoor";
+$Name = "Sohail Ahmed";   // << This is different variable 
+$income = 200;
+
+echo "This guy is $name and his income is $income Rupess <br>";
+
+echo "$name is good boy <br>";
+echo "$name is Software Engineer";
+?>
